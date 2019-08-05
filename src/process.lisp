a3fdb5db1978f4a031e29dc92175d403ceca95ac
Selwyn Simsek <sgs16@ic.ac.uk>
2019-08-05 21:34:26 +0100

    Clean up src/process.lisp

--- src/process.lisp
+++ src/process.lisp
@@ -1,17 +1,13 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (in-package :lisp-o-motive)
 
-(defvar *current-message*)
-
-(defun frame-body-as-json (frame)
-  (jsown:parse (cl-stomp:frame-body frame)))
+(defun frame-body-as-json (frame) (jsown:parse (cl-stomp:frame-body frame)))
 
 (defun parse-train-describer (frame)
   (loop for message in (frame-body-as-json frame)
-        collecting
-        (parse-train-describer-message (cadr message))))
+        collecting (parse-train-describer-message (cadr message))))
 
-(defun lispify-name (name)
-  (cl-strings:replace-all name "_" "-"))
+(defun lispify-name (name) (cl-strings:replace-all name "_" "-"))
 
 (defun message-alist-to-lambda-list (plist)
   (alexandria:flatten
@@ -32,17 +28,12 @@
           ("CC_MSG" . berth-interpose)
           ("CT_MSG" . heartbeat))))
   (defun parse-train-describer-message (message)
-    (let ((type (cdr (assoc (car message)
-                            message-types
-                            :test #'string=)))
-          (message-alist (remove-if (lambda (acons)
-                                      (string= (car acons) "msg_type")) 
-                                    (cddr message))))
+    (let ((type (cdr (assoc (car message) message-types :test #'string=)))
+          (message-alist (remove-if (lambda (acons) (string= (car acons) "msg_type")) (cddr message))))
       (apply #'make-instance type (message-alist-to-lambda-list message-alist)))))
 
 (defun process-frame (frame)
-  (setf *current-message* frame)
   (let ((destination (cl-stomp:get-destination frame)))
     (cond ((cl-strings:starts-with destination "/topic/TD")
-           (mapcar #'message-recieved (parse-train-describer frame)))
+           (map nil #'message-recieved (parse-train-describer frame)))
           (t nil))))
