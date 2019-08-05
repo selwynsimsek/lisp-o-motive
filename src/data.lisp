115b6444ef29703e3deb7ab4c8657a137b07d9c1
Selwyn Simsek <sgs16@ic.ac.uk>
2019-08-05 13:54:14 +0100

    Clean up data.lisp

--- src/data.lisp
+++ src/data.lisp
@@ -1,20 +1,14 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (in-package :lisp-o-motive)
 
 (defun project-base-directory ()
-  (make-pathname :defaults
-                 #.(or *compile-file-truename* *load-truename*)
-                 :type nil
-                 :name nil
-                 :version nil))
+  (make-pathname :defaults #.(or *compile-file-truename* *load-truename*)
+                 :type nil :name nil :version nil))
 
 (defun project-relative-pathname (relative-spec)
-  (truename
-   (merge-pathnames
-    relative-spec
-    (project-base-directory))))
+  (truename (merge-pathnames relative-spec (project-base-directory))))
 
-(defun stanme-csv-pathname ()
-  (project-relative-pathname #p"../data/stanme-codes.csv"))
+(defun stanme-csv-pathname () (project-relative-pathname #p"../data/stanme-codes.csv"))
 
 (defvar *stanme-codes*
   (let ((raw (cl-csv:read-csv (stanme-csv-pathname) :skip-first-p t))
@@ -24,8 +18,7 @@
                                  (second row))) raw)
     hash-table))
 
-(defun area-ids-pathname ()
-  (project-relative-pathname #p"../data/area-ids.csv"))
+(defun area-ids-pathname () (project-relative-pathname #p"../data/area-ids.csv"))
 
 (defvar *area-ids*
   (let ((raw (cl-csv:read-csv (area-ids-pathname) :skip-first-p t))
@@ -37,11 +30,7 @@
     hash-table))
 
 (defun berth-data-pathname ()
-  (truename
-   (merge-pathnames
-    #p"../data/out.json"
-    #.(or *compile-file-truename* *load-truename*))))
+  (project-relative-pathname #p"../data/out.json"))
 
 (defun berth-data ()
-  (cdr (jsown:parse (alexandria:read-file-into-string
-                     (berth-data-pathname)))))
+  (cdr (jsown:parse (alexandria:read-file-into-string (berth-data-pathname)))))
